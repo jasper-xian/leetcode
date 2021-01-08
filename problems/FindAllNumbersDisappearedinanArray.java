4e27cb3b0a7a4d58ef75dad2f3e2181bd85c4b7d
jasper-xian <41269031+jasper-xian@users.noreply.github.com>
2021-01-07 23:40:30 -0500

    Create FindAllNumbersDisappearedinanArray.java

---
+++ problems/FindAllNumbersDisappearedinanArray.java
@@ -0,0 +1,26 @@
+/*
+Given an array of integers where 1 ≤ a[i] ≤ n (n = size of array), some elements appear twice and others appear once.
+
+Find all the elements of [1, n] inclusive that do not appear in this array.
+
+Could you do it without extra space and in O(n) runtime? You may assume the returned list does not count as extra space.
+*/
+
+import java.util.Arrays;
+import java.util.ArrayList;
+class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        List<Integer> list = new ArrayList<Integer>();
+        
+        for (int i=0; i<nums.length; i++) {
+            if (nums[Math.abs(nums[i])-1] > 0)
+                nums[Math.abs(nums[i])-1] = -nums[Math.abs(nums[i])-1];
+        }
+        
+        for (int i=0; i<nums.length; i++) {
+            if (nums[i]>0)
+                list.add(i+1);
+        }
+        return list;
+    }
+}
